b993c5188d102e81e12d9ed05f7f79b55a37fe3c
Терещенко Владимир Дмитриевич <vd.tereshchenko@gbstudios.ru>
2015-09-22 12:37:35 +0300

    заготовка для экшинов + метод register_client

--- server/auth.js
+++ server/auth.js
@@ -1,9 +1,46 @@
 var express = require('express');
+var utils = require('./utils');
+var db = require('./db');
 
 var router = express.Router();
 
 router.get('/', function (req, res) {
     res.send('auth');
 });
+
+
+router.post('/register_client', function (req, res) {
+    var clientId = utils.uid();
+    var clientSecret = utils.token();
+    db.clients.save(clientId, clientSecret, req.body.user_data);
+
+    res.json({id: clientId, secret: clientSecret});
+});
+
+router.post('/register', function (req, res) {
+    res.send('auth');
+});
+
+
+router.post('/login', function (req, res) {
+    res.send('auth');
+});
+
+router.post('/refresh', function (req, res) {
+    res.send('auth');
+});
+
+
+/**
+ * TODO вспомогательные урлы
+ *
+/change_password
+/resend_email
+ /verify
+ /restore
+ router.post('/login-otp', function (req, res) {
+    res.send('auth');
+ });
+*/
 
 module.exports = router;
