a62aaec94a5efbe5e0006f171ac596870c484471
Терещенко Владимир Дмитриевич <vd.tereshchenko@gbstudios.ru>
2015-09-22 12:36:39 +0300

    модуль utils

--- server/utils.js
+++ server/utils.js
@@ -1,37 +1,29 @@
-/*jslint node: true */
-/*global exports */
-'use strict';
+var crypto = require('crypto');
+var uuid = require('node-uuid');
 
-/**
- * Return a unique identifier with the given `len`.
- *
- *     utils.uid(10);
- *     // => "FDaS435D2z"
- *
- * @param {Number} len
- * @return {String}
- * @api private
- */
-exports.uid = function (len) {
-  var buf = [];
-  var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var charlen = chars.length;
 
-  for (var i = 0; i < len; ++i) {
-    buf.push(chars[getRandomInt(0, charlen - 1)]);
-  }
-
-  return buf.join('');
+module.exports.uid = function () {
+    return uuid.v4();
 };
 
-/**
- * Return a random int, used by `utils.uid()`
- *
- * @param {Number} min
- * @param {Number} max
- * @return {Number}
- * @api private
- */
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+module.exports.token = function () {
+    return crypto.randomBytes(32).toString('hex');
+};
+
+
+/** хеш сумма от пароля для его хранения */
+module.exports.getHash = function (password) {
+    return crypto.createHash('sha512').update(password).digest('hex');
+};
+
+
+/** генерация токена (часть url) для подтверждения по email */
+module.exports.emailToken = function (user_str) {
+    //рандом + timestamp + e-mail + соль
+    var shasum = crypto.createHash('sha512');
+    shasum.update(Math.floor((Math.random() * 100) + 137).toString());
+    shasum.update(Date.now().toString());
+    shasum.update(user_str);
+    shasum.update("dsfsdf34rr34cvy6bh567h56756354g5g3g5345345v4e5645645ff5r6b546546bb 456rtg;sfpoebg;a");
+    return shasum.digest('hex');
+};
