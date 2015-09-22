510a1b36e1d68044002b6ea1b6e2e25c548a83af
Терещенко Владимир Дмитриевич <vd.tereshchenko@gbstudios.ru>
2015-09-22 10:50:29 +0300

    каркас сервера

---
+++ server/app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.raw());
+
+// require('./auth_strategies');
+app.use(passport.initialize());
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/api', function (req, res) {
+    res.send('API is running');
+});
+
+app.use('/api/auth', require('./auth'));
+
+
+
+var server = app.listen(3667);
+
+console.log('Server running at http://localhost:3667/');
