42f83fa05ea82500f9e950ed90d5227f34892a4a
Терещенко Владимир Дмитриевич <vd.tereshchenko@gbstudios.ru>
2015-11-03 13:01:00 +0300

    Добавил поля hasPassword facebook в userData

--- server/auth/helper.js
+++ server/auth/helper.js
@@ -1,12 +1,16 @@
 module.exports.getUserData = function (accessToken, refreshToken, expiresIn, user) {
     return {
         userId: user.userId,
+
         accessToken: accessToken,
         refreshToken: refreshToken,
         expiresIn: expiresIn,
 
         email: user.email,
         username: user.username,
-        userData: user.data
+        userData: user.data,
+        hasPassword: Boolean(user.hashPassword), // есть ли пароль, пароля может не быть если пользователь вошел через социальную сеть. показывает отображать ли блок смены пароля в профиле
+
+        facebook: user.facebook
     };
 };
