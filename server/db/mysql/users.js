4a0dd9267718b4d7afcc8643c5cb57ccce545136
Терещенко Владимир Дмитриевич <vd.tereshchenko@gbstudios.ru>
2015-11-09 12:37:08 +0300

    QGDP-16 mysql/users

--- server/db/mysql/users.js
+++ server/db/mysql/users.js
@@ -1,9 +1,30 @@
+var utils = require('./../../libs/utils');
 var sqlPool = require('./index').pool;
 
 var query = {};
 query.save = 'insert into users(userId, email, username, hashPassword, registerDate, data) values (?,?,?,?,?,?);';
-query.find = 'select userId, email, username, hashPassword, registerDate, data, facebook from users where userId = ?';
-query.findByEmail = 'select userId, email, username, hashPassword, registerDate, data, facebook from users where email = ?';
+query.find = 'select userId, email, username, hashPassword, registerDate, data, facebook, twoFactor from users where userId = ?';
+query.findByEmail = 'select userId, email, username, hashPassword, registerDate, data, facebook, twoFactor from users where email = ?';
+query.delete = 'CALL spDeleteUser(?);';
+query.setTwoFactor = 'update users set twoFactor = ? where userId = ?;';
+query.disableTwoFactor = 'update users set twoFactor = null where userId = ?;';
+query.setPassword = 'update users set hashPassword = ? where userId = ?;';
+query.socialUnlink = 'update users set ?? = null where userId = ?;';
+query.socialFind = 'select userId, email, username, hashPassword, registerDate, data, facebook, twoFactor from users where ?? = ?';
+query.socialLink = 'update users set ?? = ? where userId = ?;';
+query.socialSave = 'insert into users(userId, username, registerDate, data, ??) values (?,?,?,?,?);';
+
+function getDataFromRow(row) {
+    return {
+        userId: row.userId,
+        email: row.email,
+        username: row.username,
+        hashPassword: row.hashPassword,
+        registerDate: row.registerDate,
+        data: JSON.parse(row.data),
+        twoFactor: JSON.parse(row.twoFactor)
+    };
+}
 
 module.exports.save = function (id, email, username, hashPassword, data, done) {
     sqlPool.execute(query.save, [id, email, username, hashPassword, new Date(), JSON.stringify(data)], function (err, result) {
@@ -35,14 +56,12 @@ module.exports.find = function (id, done) {
             return;
         }
 
-        done(null, {
-            userId: rows[0].userId,
-            email: rows[0].email,
-            username: rows[0].username,
-            hashPassword: rows[0].hashPassword,
-            registerDate: rows[0].registerDate,
-            data: JSON.parse(rows[0].data)
-        });
+        if (rows.length === 0) {
+            done(null, null);
+            return;
+        }
+
+        done(null, getDataFromRow(rows[0]));
     });
 };
 
@@ -58,29 +77,123 @@ module.exports.findByEmail = function (email, done) {
             return;
         }
 
-        done(null, {
-            userId: rows[0].userId,
-            email: rows[0].email,
-            username: rows[0].username,
-            hashPassword: rows[0].hashPassword,
-            registerDate: rows[0].registerDate,
-            data: JSON.parse(rows[0].data)
-        });
+        done(null, getDataFromRow(rows[0]));
     });
 };
 
+/**
+ * используется в процессе регистрации для отката, если возникают ошибки при сохранении accessToken или refreshToken
+ */
 module.exports.delete = function (id, done) {
-
+    sqlPool.execute(query.delete, [id], function (err) {
+        if (typeof done === 'function') {
+            done(err);
+        }
+    });
 };
 
 module.exports.setTwoFactor = function (id, twoFactor, done) {
-
+    sqlPool.execute(query.setTwoFactor, [JSON.stringify(twoFactor), id], function (err) {
+        if (typeof done === 'function') {
+            done(err);
+        }
+    });
 };
 
 module.exports.disableTwoFactor = function (id, done) {
-
+    sqlPool.execute(query.disableTwoFactor, [id], function (err) {
+        if (typeof done === 'function') {
+            done(err);
+        }
+    });
 };
 
 module.exports.setPassword = function (id, hashPassword, done) {
+    sqlPool.execute(query.setPassword, [hashPassword, id], function (err) {
+        if (typeof done === 'function') {
+            done(err);
+        }
+    });
+};
 
+module.exports.social = {
+    /**
+     * login or register
+     */
+    signin: function (social, profileId, username, data, done) {
+        this.find(social, profileId, function (err, user) {
+            if (err) {
+                done(err);
+                return;
+            }
+
+            if (user) {
+                done(null, user);
+                return;
+            }
+
+            var userId = utils.uid();
+            sqlPool.execute(query.socialSave, [social, userId, username, new Date(), JSON.stringify(data), profileId], function (err, result) {
+                if (err) {
+                    done(err);
+                    return;
+                }
+
+                var newUser = {
+                    userId: userId,
+                    username: username,
+                    registerDate: new Date(),
+                    data: data
+                };
+                newUser[social] = profileId;
+
+                done(null, newUser);
+            });
+        });
+    },
+
+    link: function (id, social, profileId, done) {
+        this.find(social, profileId, function (err, user) {
+            if (err) {
+                done(err);
+                return;
+            }
+
+            if (user) {
+                // этот аккаунт привязан к другому пользователю
+                done(new Error("EXISTS"));
+                return;
+            }
+
+            sqlPool.execute(query.socialFind, [social, profileId, id], function (err) {
+                if (typeof done === 'function') {
+                    done(null);
+                }
+            });
+        });
+    },
+
+    unlink: function (id, social, done) {
+        sqlPool.execute(query.socialUnlink, [social, id], function (err) {
+            if (typeof done === 'function') {
+                done(err);
+            }
+        });
+    },
+
+    find: function (social, profileId, done) {
+        sqlPool.execute(query.socialUnlink, [social, profileId], function (err, rows) {
+            if (err) {
+                done(err);
+                return;
+            }
+
+            if (rows.length === 0) {
+                done(null, null);
+                return;
+            }
+
+            done(null, getDataFromRow(rows[0]));
+        });
+    }
 };
