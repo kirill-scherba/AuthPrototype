2338d8754747e25ed7bdf81fca98ffd2ef624c0a
Терещенко Владимир Дмитриевич <vd.tereshchenko@gbstudios.ru>
2015-12-03 11:19:47 +0300

    Remove yandex cocaine dependence from db/mysql/index.js

--- server/db/mysql/index.js
+++ server/db/mysql/index.js
@@ -1,7 +1,6 @@
 var mysql = require('mysql2');
 var config = require("./../../libs/config");
 
-var mysql_config_name = process.env.NODE_ENV === 'cocaine' ? 'mysql_cocaine' : 'mysql';
-var pool = mysql.createPool(config.get(mysql_config_name));
+var pool = mysql.createPool(config.get('mysql'));
 
 module.exports.pool = pool;
