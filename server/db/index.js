10b7ba876edd0423a9080659027596c1cb672bb3
Терещенко Владимир Дмитриевич <vd.tereshchenko@gbstudios.ru>
2015-12-03 16:12:14 +0300

    Changed exports to module.exports in db/index.js

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,11 @@
 var config = require('./../libs/config');
 var storage = config.get('storage');
 
-exports.users = require('./' + storage + '/users');
-exports.clients = require('./' + storage + '/clients');
-exports.accessTokens = require('./' + storage + '/accesstokens');
-exports.refreshTokens = require('./' + storage + '/refreshtokens');
-exports.temporaryTokens = require('./' + storage + '/temporarytokens');
-exports.emailValidation = require('./' + storage + '/emailvalidation');
-exports.emailRestore = require('./' + storage + '/emailrestore'); // TODO not used because the new password is sent by email
-exports.socialTemporaryTokens = require('./' + storage + '/socialtemporarytokens');
+module.exports.users = require('./' + storage + '/users');
+module.exports.clients = require('./' + storage + '/clients');
+module.exports.accessTokens = require('./' + storage + '/accesstokens');
+module.exports.refreshTokens = require('./' + storage + '/refreshtokens');
+module.exports.temporaryTokens = require('./' + storage + '/temporarytokens');
+module.exports.emailValidation = require('./' + storage + '/emailvalidation');
+module.exports.emailRestore = require('./' + storage + '/emailrestore'); // TODO not used because the new password is sent by email
+module.exports.socialTemporaryTokens = require('./' + storage + '/socialtemporarytokens');
